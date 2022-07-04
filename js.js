57b990f701e7eaccbb82acc8abbeb5f30a93e7b9
samuka-dev <patofxcontato@gmail.com>
2022-07-04 20:18:38 -0300

    Meu primeiro commit

---
+++ js.js
@@ -0,0 +1,16 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.section');
+    sec.style.background = color;
+}
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+   
